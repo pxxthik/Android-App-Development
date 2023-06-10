af8991ca96117ee72f470799a3ea2533d6fbb9e6
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-06-10 20:16:19 +0530

    methods in java

---
+++ java-programs/methods.java
@@ -0,0 +1,11 @@
+public class methods{
+    public static void main(String[] args){
+
+        int sum = add(100, 400);
+        System.out.println("The sum is: "+ sum);
+    }
+
+    public static int add(int num1, int num2){
+        return num1+ num2;
+    }
+}
