ab89dc1740dc1d588b9b49d5b06b64db5c6e7275
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-06-10 10:20:21 +0530

    variables in java

---
+++ java-programs/variables.java
@@ -0,0 +1,10 @@
+class variables{
+    public static void main(String[] args){
+        int num1;
+        num1 = 7;
+        int num2 = 21;
+
+        int sum = num1 + num2;
+        System.out.println("The sum is: "+ sum);
+    }
+}
