ea41a5749fa46f6fa0a13e632d4777aa487a30cb
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-06-18 12:31:20 +0530

    splash screen

---
+++ SplashScreen/app/src/main/java/com/sxnu/splashscreen/splashActivity.java
@@ -0,0 +1,26 @@
+package com.sxnu.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent Home = new Intent(splashActivity.this, MainActivity.class);
+                startActivity(Home);
+                finish();
+            }
+        }, 3000);
+
+    }
+}
