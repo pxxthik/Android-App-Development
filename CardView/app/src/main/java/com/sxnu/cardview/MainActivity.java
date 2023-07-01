6f4a5de2b857eb94b42b81c95ba020cd102d082a
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-07-01 22:12:31 +0530

    card view in android

---
+++ CardView/app/src/main/java/com/sxnu/cardview/MainActivity.java
@@ -0,0 +1,21 @@
+package com.sxnu.cardview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    CardView cardView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cardView = findViewById(R.id.cardView);
+        cardView.setCardElevation(12.0f);
+
+    }
+}
